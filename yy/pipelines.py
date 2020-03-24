b2d7d0e666d96259ed80ce86b97741a155780b1e
fuzz <732239430@qq.com>
2020-03-24 22:43:33 +0800

    Update pipelines.py

--- yy/pipelines.py
+++ yy/pipelines.py
@@ -24,17 +24,17 @@ class MysqlPipeline(object):
     """
     def __init__(self):
         # 建立连接
-        self.conn = pymysql.connect(host='192.168.123.100',port=32109,user='root',password='qzk7lWeyL4',db='spider',charset='utf8')
+        self.conn = pymysql.connect(host='192.168.123.100',port=32109,user='root',password='qzk7lWeyL4',db='spider',charset='utf8')  # 有中文要存入数据库的话要加charset='utf8'
         # 创建游标
         self.cursor = self.conn.cursor()
  
     def process_item(self,item,spider):
         # sql语句
         insert_sql = """
-        insert into yy(id,province,city,region,name,level,character,contact) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)
+        insert into yy (`id`,`province`,`city`,`region`,`name`,`level`,`character`,`contact`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
         """
         # 执行插入数据到数据库操作
-        self.cursor.execute(insert_sql,(item['province'],item['city'],item['region'],item['name'],item['level'],item['character'],item['contact']))
+        self.cursor.execute(insert_sql,(str(item['id']),str(item['province']),str(item['city']),str(item['region']),str(item['name']),str(item['level']),str(item['character']),str(item['contact'])))
         # 提交，不进行提交无法保存到数据库
         self.conn.commit()
  
