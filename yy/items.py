c236a9820815e256773fb6c97dea9378677442ad
fuzz <732239430@qq.com>
2020-03-24 22:46:23 +0800

    Update items.py

--- yy/items.py
+++ yy/items.py
@@ -11,6 +11,7 @@ import scrapy
 class YyItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    id = scrapy.Field()
     #省
     province = scrapy.Field()
     #市
